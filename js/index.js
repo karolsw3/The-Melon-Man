3c7cc038cc368212debad1f4124ab7bf1431d15e
Karol Świerczek <karol@swierczek.pro>
2018-12-13 15:14:09 +0100

    Add basic map generating

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
+
+
+
 var game = {
 	canvas: document.getElementById('canvas'),
 	context: this.canvas.getContext('2d'),
 	textures: new Image(),
+	drawPending: false,
 	options: {
 		texturesPath: "https://cdn.pbrd.co/images/HRx0tpd.png",
 		tileWidth: 24,
@@ -16,26 +20,60 @@ var game = {
 		this.textures.src = this.options.texturesPath
 		this.textures.onload = onInit
 	},
+	map: {
+		structures: []
+	},
 	animations: {
+		// Describe coordinates of consecutive animation frames of objects in textures
 		player: {
-			left: [{x: 4, y: 0} {x: 5, y: 0}, {x: 4, y: 0}, {x: 6, y: 0}],
-			right: [{x: 9, y: 0}, {x: 8, y: 0}, {x: 9, y: 0}, {x: 7, y: 0}]
+			left: [{tileColumn: 4, tileRow: 0}, {tileColumn: 5, tileRow: 0}, {tileColumn: 4, tileRow: 0}, {tileColumn: 6, tileRow: 0}],
+			right: [{tileColumn: 9, tileRow: 0}, {tileColumn: 8, tileRow: 0}, {tileColumn: 9, tileRow: 0}, {tileColumn: 7, tileRow: 0}]
 		}
+	},
+	// Describe structures using coordinates of textures and coordinates on map
+	structures: {
+		grassPlatform: [{tileColumn: 0, tileRow: 0, x: 0, y: 0}, {tileColumn: 1, tileRow: 0, x: 1, y: 0}, {tileColumn: 2, tileRow: 0, x: 2, y: 0}]
 	},
 	drawTile: function (tileColumn, tileRow, x, y) {
 		this.context.drawImage(this.textures, tileColumn * this.options.tileWidth, tileRow * this.options.tileHeight, this.options.tileWidth, this.options.tileHeight, x * this.options.tileWidth, y * this.options.tileHeight, this.options.tileWidth, this.options.tileHeight)
-	}
-	redraw: function (timestamp) {
-		drawPending = false
-		// Draw canvas
-		this.context.fillStyle = "#a5d9ff";
+	},
+	drawStructure: function (structureName, x, y) {
+		var structure = this.structures[structureName]
+		for (var i = 0; i < structure.length; i++) {
+			this.drawTile(structure[i].tileColumn, structure[i].tileRow, structure[i].x + x, structure[i].y + y)
+		}
+	},
+	generateMap: function () {
+		var height = 50
+		for (var i = 0; i < height; i++) {
+			this.map.structures.push({
+				structureName: "grassPlatform",
+				x: Math.floor(Math.random() * 10 + 2),
+				y: i * 3
+			})
+		}
+	},
+	redraw: function () {
+		this.drawPending = false
+
+		// Draw the background
+		this.context.fillStyle = "#a5d9ff"
 		this.context.fillRect(0, 0, this.canvas.width, this.canvas.height)
+
+		// Draw the map
+		for (var i = 0; i < this.map.structures.length; i++) {
+			this.drawStructure(this.map.structures[i].structureName, this.map.structures[i].x, this.map.structures[i].y)
+		}
 	},
 	requestRedraw: function () {
-		if (!drawPending) {
-			drawPending = true
-			requestAnimationFrame(this.redraw)
+		if (!this.drawPending) {
+			this.drawPending = true
+			requestAnimationFrame(this.redraw.bind(this))
 		}
 	}
 }
 
+game.init(function () {
+	game.generateMap()
+	game.requestRedraw()
+})
